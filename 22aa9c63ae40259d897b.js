b30bef64c176c74789973ccb8b11f456372ff1d0
zhiguang <zhiguang53846@gmail.com>
2024-09-17 22:05:08 -0400

    finished the assignment

--- 22aa9c63ae40259d897b.js
+++ 22aa9c63ae40259d897b.js
@@ -7,6 +7,8 @@ import photo6 from "./photo-1726180834320-63ebdc3b7548.avif";
 import photo7 from "./photo-1726486896376-4d1340e2f672.avif";
 import rightArrow from "./chevron-right.svg";
 import leftArrow from "./chevron-left.svg";
+import filledCircle from "./circle.svg";
+import emptyCircle from "./circle-outline.svg";
 import "./style.css";
 
 const init = (() => {
@@ -72,64 +74,93 @@ const init = (() => {
     image6.style.display = 'none';
     photos.push(image6);
 
+    const circles = document.querySelector('.circles');
+
     for (const image of photos) {
         images.appendChild(image);
+
+        const empty = document.createElement('img');
+        empty.src = emptyCircle;
+        empty.classList.add('emptyCircle');
+        const filled = document.createElement('img');
+        filled.src = filledCircle;
+        filled.classList.add('filledCircle');
+        filled.style.display = 'none';
+        circles.appendChild(empty);
+        circles.appendChild(filled);
     }
 
-    const left = document.createElement('img');
-    left.src = leftArrow;
-    left.width = '50';
-    left.style.opacity = '0.5';
-    const right = document.createElement('img');
-    right.src = rightArrow;
-    right.width = '50';
-    right.style.opacity = '0.5';
-
-    frame.appendChild(left);
-    frame.appendChild(right);
+    const emptyCircles = document.querySelectorAll('.emptyCircle');
+    const filledCircles = document.querySelectorAll('.filledCircle');
+    const photoList = document.querySelectorAll('.photo');
 
     let currPhoto = null;
     let prevPhoto = null;
 
-    const photoList = document.querySelectorAll('.photo');
+    for (let i = 0; i < emptyCircles.length; i++) {
+        emptyCircles[i].addEventListener('click', (event) => {
+            prevPhoto = null;
+            currPhoto = i;
+            filledCircles[i].style.display = 'flex';
+            emptyCircles[i].style.display = 'none';
+            for (let j = 0; j < photoList.length; j++) {
+                if (photoList[j].style.display === 'flex') {
+                    prevPhoto = j;
+                    emptyCircles[j].style.display = 'flex';
+                    filledCircles[j].style.display = 'none';
+                    break;
+                }
+                else {
+                    prevPhoto = null;
+                }
+            }
+            displayPhoto(prevPhoto, currPhoto);
+        })
+    }
+
+    const left = document.createElement('img');
+    left.src = leftArrow;
+    left.classList.add('icon');
+    const right = document.createElement('img');
+    right.src = rightArrow;
+    right.classList.add('icon');
+
+    frame.appendChild(left);
+    frame.appendChild(right);
 
     left.addEventListener('click', () => {
-        if (currPhoto === null) {
-            currPhoto = 0;
-            displayPhoto(prevPhoto, currPhoto);
-        }
-        else {
-            prevPhoto = currPhoto;
-            currPhoto--;
-            if (currPhoto === -1) {
-                currPhoto = photoList.length - 1;
-                displayPhoto(prevPhoto, currPhoto);
-            }
-            else {
-                displayPhoto(prevPhoto, currPhoto);
+        let prev = null;
+        let curr = null;
+        for (let i = 0; i < photoList.length; i++) {
+            if (photoList[i].style.display === 'flex') {
+                prev = i;
+                curr = --i;
+                break;
             }
         }
+        if (curr === -1 || curr === null) {
+            curr = photoList.length - 1;
+        }
+        displayPhoto(prev, curr);
     })
 
     right.addEventListener('click', () => {
-        if (currPhoto === null) {
-            currPhoto = 0;
-            displayPhoto(prevPhoto, currPhoto);
-        }
-        else {
-            prevPhoto = currPhoto;
-            currPhoto++;
-            if (currPhoto === photoList.length) {
-                currPhoto = 0;
-                displayPhoto(prevPhoto, currPhoto);
-            }
-            else {
-                displayPhoto(prevPhoto, currPhoto);
+        let prev = null;
+        let curr = null;
+        for (let i = 0; i < photoList.length; i++) {
+            if (photoList[i].style.display === 'flex') {
+                prev = i;
+                curr = ++i;
+                break;
             }
         }
+        if (curr === photoList.length || curr === null) {
+            curr = 0;
+        }
+        displayPhoto(prev, curr);
     })
 
-    return { frame, left, right, photoList, currPhoto };
+    return { frame, right, photoList, emptyCircles, filledCircles };
 
 })();
 
@@ -138,13 +169,19 @@ function displayPhoto(prevPhoto, currPhoto) {
     if (prevPhoto !== null && currPhoto !== null) {
         const current = init.photoList[currPhoto];
         current.style.display = 'flex';
+        init.filledCircles[currPhoto].style.display = 'flex';
+        init.emptyCircles[currPhoto].style.display = 'none';
         const previous = init.photoList[prevPhoto];
         previous.style.display = 'none';
+        init.emptyCircles[prevPhoto].style.display = 'flex';
+        init.filledCircles[prevPhoto].style.display = 'none';
         init.frame.insertBefore(current, init.right);
     }
     else {
         const current = init.photoList[currPhoto];
         current.style.display = 'flex';
+        init.emptyCircles[currPhoto].style.display = 'none';
+        init.filledCircles[currPhoto].style.display = 'flex';
         init.frame.insertBefore(current, init.right);
     }
 }
